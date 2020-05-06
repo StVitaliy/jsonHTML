4dae37d62e38ee38642a871d5cd05b2183a878de
StVitaliy <64489694+StVitaliy@users.noreply.github.com>
2020-05-06 07:00:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-/*const http=require('http');
-
-http.createServer(function(req,res){
-res.write('hello node.js')
-res.end();
-}).listen(3000,function(){
-    console.log('Server at http://localhost:3000');
-})
-
- 
- }
- xhr.send() */
-
-
  const requestURL='https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3'
  let request = new XMLHttpRequest();
  request.open('GET', requestURL);
